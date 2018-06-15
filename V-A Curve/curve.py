57753ec535506b7568da83030af8ea8d55696d01
杨家玺 <1468764755@qq.com>
2018-06-15 21:30:54 +0800

    <MOD> change the way of plot show

--- V-A Curve/curve.py
+++ V-A Curve/curve.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def il(str):
     res = []
     for ele in str.split('/'):
@@ -9,6 +10,30 @@ def il(str):
         return res[0]
     else:
         return res
+
+
+def rd(mr, mn, K, _data):
+    data = np.array(_data)
+    r = mr * K / 100
+    if r < 1:
+        for i in range(1, 10):
+            if round(r, i) > 0:
+                r_d = i
+                break
+    else:
+        raise ValueError("not less than 1!")
+    return np.round(data / mn * mr, r_d)
+
+
+def show_data(data, name=None):
+    if name is None:
+        name = [''] * len(data)
+    for (e, n) in zip(data, name):
+        print(n, end=': ')
+        for ee in e:
+            print(ee, end=' ')
+        print()
+
 
 def exp1(_iU, _iI, _oU, _oI):
     iU = np.append(_iU, 0)
@@ -27,33 +52,37 @@ def exp1(_iU, _iI, _oU, _oI):
         ms.append(round(m, 2))
         cs.append(round(c, 2))
         plt.scatter(a[:-1], b[:-1], c='black', s=40, marker=s, label=n)
-    lbs = ["内接法 "+"$y={}x+{}$".format(ms[0], cs[0]), 
-           "外接法 "+"$y={}x+{}$".format(ms[1], cs[1])]
+
+    iR = round(1000 / ms[0], 2)
+    oR = round(1000 / ms[1], 2)
+    lbs = ["内接法 $y={}x{}{},R={}$".format(ms[0], '+' if cs[0] > 0 else '', cs[0], iR),
+           "外接法 $y={}x{}{},R={}$".format(ms[1], '+' if cs[1] > 0 else '', cs[1], oR)]
     plt.legend(lbs, frameon=True, fancybox=True, shadow=True)
-    
+
     for (a, b) in zip(Ul, Il):
         A = np.vstack([a, np.ones(len(a))]).T
         m, c = np.linalg.lstsq(A, np.array(b), rcond=1)[0]
-        plt.plot(iU, m * a + c, c='black')
-        
+        plt.plot(a, m * a + c, c='black')
+
     plt.title("定值电阻伏安特性曲线")
     plt.xlabel("$U/V$")
     _ = plt.ylabel("$I/mA$")
-    
+
 
 def exp2(_Ua, _Ia, _Ub, _Ib):
     Ua = np.array(_Ua)
     Ia = np.array(_Ia)
     Ub = 0 - np.array(_Ub)
     Ib = 0 - np.array(_Ib)
-    U = np.sort(np.unique(np.concatenate((Ua, Ub))))
-    I = np.sort(np.unique(np.concatenate((Ia, Ib))))
+    U = np.sort(np.concatenate((Ua, Ub)))
+    I = np.sort(np.concatenate((Ia, Ib)))
     plt.scatter(U, I, color='black', s=30)
     plt.plot(U, I, color='black')
     plt.title("半导体二极管2CW52的正、反向伏安特征曲线")
     plt.xlabel("$U/V$")
     _ = plt.ylabel("$I/mA$")
-    
+
+
 def exp3(_U40, _I40, _U60, _I60, _U80, _I80):
     U40 = np.array(_U40)
     I40 = np.array(_I40)
@@ -63,7 +92,7 @@ def exp3(_U40, _I40, _U60, _I60, _U80, _I80):
     I80 = np.array(_I80)
     Ul = [U40, U60, U80]
     Il = [I40, I60, I80]
-    mks = ['o','s','^']
+    mks = ['o', 's', '^']
     lbs = ["$40μA$", "$60μA$", "$80μA$"]
     for (a, b, s, n) in zip(Ul, Il, mks, lbs):
         # plt.plot(a, b, color='black', markevery=100)
@@ -73,6 +102,6 @@ def exp3(_U40, _I40, _U60, _I60, _U80, _I80):
     Imax = int(np.max([I40.max(), I60.max(), I80.max()])) + 1
     plt.xlabel("$U/V$")
     plt.ylabel("$I/mA$")
-    plt.xticks(np.linspace(0,Umax,1+int(Umax/0.5)))
-    plt.yticks(np.linspace(0,Imax,1+int(Imax)))
+    plt.xticks(np.linspace(0, Umax, 1+int(Umax/0.5)))
+    plt.yticks(np.linspace(0, Imax, 1+int(Imax)))
     _ = plt.title("晶体三极管的输出特征曲线")
